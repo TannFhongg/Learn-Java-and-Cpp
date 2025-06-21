5750bd65370b4ea573ca9bab7753936b6541dc2c
TannFhongg <tuanvv.21ce@vku.udn.vn>
2025-06-21 15:27:36 +0700

    OOP_Inheritance

--- OOP_Inheritance.cpp
+++ OOP_Inheritance.cpp
@@ -24,13 +24,15 @@ public:
     {
         this->address = address;
     }
-    void in() {
-        cout << name << " " << address << " "; 
+    void in()
+    {
+        cout << name << " " << address << " ";
     }
 
-    Person(string name,string address) {
-        this -> name = name; 
-        this -> address = address; 
+    Person(string name, string address)
+    {
+        this->name = name;
+        this->address = address;
     }
 };
 
@@ -48,23 +50,76 @@ public:
     {
         this->gpa = gpa;
     }
-    void in() { // function overriding
-        Person::in(); 
-        cout << gpa << fixed << setprecision(2) << endl; 
+    void in()
+    { // function overriding
+        Person::in();
+        cout << gpa << fixed << setprecision(2) << endl;
     }
-    Student(string name,string address, double gpa) : Person( name, address) {
-        this -> gpa = gpa; 
+    Student(string name, string address, double gpa) : Person(name, address)
+    {
+        this->gpa = gpa;
     }
 };
 
+class A
+{
+public:
+    A()
+    {
+        cout << "Constructor A \n!";
+    }
+    ~A()
+    {
+        cout << "Destructor A \n!";
+    }
+    void xinChao()
+    {
+        cout << "Hello A \n!";
+    }
+};
+class B : public A
+{
+public:
+    B()
+    {
+        cout << "Constructor B \n!";
+    }
+        ~B()
+    {
+        cout << "Destructor B \n!";
+    }
+    void xinChao()
+    {
+        cout << "Hello B \n!";
+    }
+};
+class C : public B // multi level
+{
+public:
+    C()
+    {
+        cout << "Constructor C \n!";
+    }
+        ~C()
+    {
+        cout << "Destructor C \n!";
+    }
+    void xinChao()
+    {
+        cout << "Hello C \n!";
+    }
+};
+class D : public A,public B,public C {}; //multiple inheritance
 int main()
 {
 
-    Student s("Tuanvv","QuangBinh",3.50);
+    Student s("Tuanvv", "QuangBinh", 3.50);
     // s.setName("Tuanvv");
     // s.setAddress("QuangBinh");
     // s.setGPA(3.0);
-    s.in(); 
-   // s.Person::in(); 
+    s.in();
+    // s.Person::in();
+    C obj; 
+    obj.xinChao(); 
 
 }
