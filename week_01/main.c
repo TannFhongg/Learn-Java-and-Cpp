0ca6cac52675e568f69eaf95f54e52152f347017
TannFhongg <tuanvv.21ce@vku.udn.vn>
2025-07-03 09:59:36 +0700

    week_01

---
+++ week_01/main.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+    
+   int x = add(4,6); 
+    printf("%d\n",x); 
+  int y = sub(4,6); 
+   printf("%d\n",y); 
+	return 0;
+}
