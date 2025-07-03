3633d7e1886d3d124d2952b690453e784796302e
TannFhongg <tuanvv.21ce@vku.udn.vn>
2025-07-03 10:09:02 +0700

    little up

--- week_01/main.c
+++ week_01/main.c
@@ -1,13 +1,14 @@
-
 #include <stdio.h>
+#include "math.h"
 
-int main(int argc, char **argv)
-{
-	printf("hello world\n");
-    
-   int x = add(4,6); 
-    printf("%d\n",x); 
-  int y = sub(4,6); 
-   printf("%d\n",y); 
-	return 0;
+int main() {
+    printf("hello world\n");
+
+    int x = add(4, 6);
+    printf("%d\n", x);
+
+    int y = sub(4, 6);
+    printf("%d\n", y);
+
+    return 0;
 }
